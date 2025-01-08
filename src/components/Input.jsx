4f038e696d3a50fa0193079dee0a4574dcfe7ce3
Emediong Bassey Edem <edememediong1@gmail.com>
2025-01-08 18:50:18 +0100

    completing the Newproject bar

---
+++ src/components/Input.jsx
@@ -0,0 +1,12 @@
+
+
+function Input({label, textarea, ...props}) {
+  return (
+    <p>
+        <label>{label}</label>
+        {textarea ? <textarea {...props} />: <input {...props} /> }
+    </p>
+  )
+}
+
+export default Input
