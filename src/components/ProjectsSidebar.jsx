1a795ab0820f0f975497981f6ab62a6419767b20
Emediong Bassey Edem <edememediong1@gmail.com>
2025-01-08 14:12:28 +0100

    adding styling to the sidebar

--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -2,11 +2,10 @@ import React from 'react'
 
 function ProjectsSidebar() {
   return (
-    <aside>
-        <h2>Your Projects</h2>
+    <aside className='w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-md'>
+        <h2 className='mb-8 font-bold uppercase md:text-xl text-stone-200'>Your Projects</h2>
         <div className="">
-
-        <button>+ Add Project</button>    
+          <button>+ Add Project</button>    
         </div>
     </aside>
   )
