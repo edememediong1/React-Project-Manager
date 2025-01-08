dc64a7857eb0d370926cb8ed69b23ea2264cd712
Emediong Bassey Edem <edememediong1@gmail.com>
2025-01-08 11:27:09 +0100

    first commit

---
+++ src/components/ProjectsSidebar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function ProjectsSidebar() {
+  return (
+    <aside>
+        <h2>Your Projects</h2>
+        <div className="">
+
+        <button>+ Add Project</button>    
+        </div>
+    </aside>
+  )
+}
+
+export default ProjectsSidebar
