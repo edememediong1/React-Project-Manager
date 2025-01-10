8a7e5f30588a5d8c81ef90d5391bae023612aaf1
Emediong Bassey Edem <edememediong1@gmail.com>
2025-01-10 04:16:32 +0100

    working on add project toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import ProjectsSidebar from "./components/ProjectsSidebar";
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+import { useState } from "react";
 
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  })
+
+  function handleStartAddProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      }
+    })
+  }
   return (
-    <main className="h-screen my-8 flex gap-8">
+    <main className="h-screen mt-8 flex gap-8">
       <ProjectsSidebar />
-      <NewProject/>
+      <NoProjectSelected/>
     </main>
   );
 }
--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -1,7 +1,14 @@
+import noProjectImg from "../assets/no-projects.png"
+import Button from "./Button"
+
 export default function NoProjectSelected(){
-    return <div>
-        return <div>
-            
+  
+        return <div className="mt-24 text-center w-2/3">
+            <img src={noProjectImg} alt="Empty task" className="w-24 m-auto"/>
+            <h2 className="text-xl font-bold text-stone-500 my-4">No project Selected</h2>
+            <p className="text-stone-400 mb-4">Select a project or get started with a new one</p>
+            <p className="mt-8">
+                <Button>Create new project</Button>
+            </p>
         </div>
-    </div>
 }
