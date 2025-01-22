37996d13379453608f0a2e503463cb611f932b60
Emediong Bassey Edem <edememediong1@gmail.com>
2025-01-22 16:52:15 +0100

    added the delete functionality

--- src/App.jsx
+++ src/App.jsx
@@ -30,6 +30,17 @@ function App() {
     })
   }
 
+  function handleDeleteProject(){
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter((project) => project.id !== prevState.selectedProjectId)
+      }
+
+    })
+  }
+
   function handleCancelAddProject(){
     setProjectsState((prevState) => {
       return {
@@ -58,7 +69,7 @@ function App() {
   console.log(projectsState)
 
   const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId)
-  let content = <SelectedProject project={selectedProject} />
+  let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject}/>
 
   if (projectsState.selectedProjectId === null) {
     content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
