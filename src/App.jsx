9d51b2e0e372d7269bb34597de3d6236718957b4
Emediong Bassey Edem <edememediong1@gmail.com>
2025-01-28 02:58:24 +0100

    delete task functionality working well

--- src/App.jsx
+++ src/App.jsx
@@ -28,8 +28,15 @@ function App() {
     })
   }
 
-  function handleDeleteTask(){
-
+  function handleDeleteTask(id){
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        tasks: prevState.tasks.filter(
+          (task) => task.id !== id
+        )
+      }
+    })
   }
 
   function handleSelectProject(id){
